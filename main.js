beba4a396d2c41a5f9c79c6682d6be0d7c5b8253
Zara-Khan-7 <zaara.khan1795.zy@gmail.com>
2024-09-11 05:10:46 +0500

    Adding sitch case

---
+++ main.js
@@ -0,0 +1,93 @@
+"use strict";
+// switch statements
+let fruits1 = 0;
+switch (fruits1) {
+    case 0:
+        console.log("i am apple");
+        break;
+    case 1:
+        console.log("i am banana");
+        break;
+    case 2:
+        console.log("i am lychee");
+        break;
+    case 3:
+        console.log("i am mango");
+        break;
+    default: {
+        console.log("invalid fruit");
+        break;
+    }
+}
+let day = 4;
+switch (day) {
+    case 0:
+        console.log("i am sunday");
+        break;
+    case 1:
+        console.log("i am monday");
+        break;
+    case 2:
+        console.log("i am tuesday");
+        break;
+    case 3:
+        console.log("i am wednesday");
+        break;
+    case 4:
+        console.log("i am thursday");
+        break;
+    case 5:
+        console.log("i am friday");
+        break;
+    case 6:
+        console.log("i am saturday");
+        break;
+    default: {
+        console.log("invalid day");
+        break;
+    }
+}
+let costume = 5;
+switch (costume) {
+    case 0:
+        rainy: console.log("wear a raincoat");
+        break;
+    case 1:
+        party: console.log("wear a dress");
+        break;
+    case 2:
+        bikeRide: console.log("wear a helmet");
+        break;
+    case 3:
+        sunnyDay: console.log("wear sunglasses");
+        break;
+    case 4:
+        expectedHailstorm: console.log("stay at home");
+        break;
+    case 5:
+        cold: console.log("wear a sweatshirt");
+        break;
+    case 6:
+        cloudy: console.log("wear a jacket");
+        break;
+    default: {
+        console.log("wear whatever you want");
+        break;
+    }
+}
+let fruits = 2;
+switch (fruits) {
+    case 0:
+        console.log("i am apple");
+        break;
+    case 1:
+        console.log("i am mango");
+        break;
+    case 2:
+        console.log("i am lychee");
+        break;
+    default: {
+        console.log("invalid fruit name");
+        break;
+    }
+}
